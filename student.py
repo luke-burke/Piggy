342037fdd462c531aa42c50ee11d58a18360e342
Luke Burke <102990983+luke-burke@users.noreply.github.com>
2022-05-26 14:16:26 +0000

    bm24

--- student.py
+++ student.py
@@ -127,14 +127,14 @@ class Piggy(PiggyParent):
             time.sleep(0.6)
             self.servo(2100)
             while self.read_distance() <= 450:
-            self.fwd()
-            time.sleep(0.5)
-            self.left()
-            time.sleep(0.6)
-            self.fwd()
-            time.sleep(1)
-            self.left()
-            time.sleep(0.6)
+              self.fwd()
+              time.sleep(0.5)
+              self.left()
+              time.sleep(0.6)
+              self.fwd()
+              time.sleep(1)
+              self.left()
+              time.sleep(0.6)
 
         
         
